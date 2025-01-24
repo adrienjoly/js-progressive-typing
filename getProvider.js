52068d08aff36c94ae2c138ae1792f1ab739e914
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-01-24 16:52:32 +0100

    fix: complete switch with missing case => no more errors!

--- getProvider.js
+++ getProvider.js
@@ -10,6 +10,8 @@ export function getProvider(speciality) {
             return { name: 'Pizza Capri', phoneNumber: '312-949-1311' };
         case 'burger':
             return { name: 'McSmash', phoneNumber: '123-242-4399' };
+        case 'sushi':
+            return { name: 'Fuji Sushi', phoneNumber: '456-728-1278' };
         default:
             // this should never happen, if speciality's value satisfies its type
             /** @satisfies {never} */(speciality);
--- getProvider.ts
+++ getProvider.ts
@@ -6,6 +6,8 @@ export function getProvider(speciality: Speciality): { name: string, phoneNumber
             return { name: 'Pizza Capri', phoneNumber: '312-949-1311' };
         case 'burger':
             return { name: 'McSmash', phoneNumber: '123-242-4399' };   
+        case 'sushi':
+            return { name: 'Fuji Sushi', phoneNumber: '456-728-1278' };
         default:
             // this should never happen, if speciality's value satisfies its type
             speciality satisfies never;
