bcc3d40516c04ef7778ddc895f49014736770ba5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-01-24 14:55:34 +0100

    initial commit: dispatchOrder.js, getProvider.js, sendOrder.js

---
+++ dispatchOrder.js
@@ -0,0 +1,10 @@
+import { getProvider } from './getProvider.js';
+import { sendOrder } from './sendOrder.js';
+
+export async function dispatchOrder(speciality, customerAddress) {
+    const provider = getProvider(speciality);
+    sendOrder(provider.phoneNumber, customerAddress);
+}
+
+// sample call
+await dispatchOrder('pizza', '55 Rue du Faubourg Saint-Honoré, 75008 Paris');
---
+++ getProvider.js
@@ -0,0 +1,10 @@
+export function getProvider(speciality) {
+    switch (speciality) {
+        case 'pizza':
+            return { name: 'Pizza Capri', phoneNumber: '312-949-1311' };
+        case 'burger':
+            return { name: 'McSmash', phoneNumber: '123-242-4399' };   
+        default:
+            console.error('unknown speciality');
+    }
+}
