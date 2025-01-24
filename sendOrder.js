b847e11bf5a98d247838ef1abe1d69c97cb095bb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-01-24 17:26:35 +0100

    new case: order fails, but frontend doesn't know

--- sendOrder.js
+++ sendOrder.js
@@ -4,6 +4,8 @@
  * @param {string} phoneNumber 
  * @param {string} customerAddress 
  */
-export function sendOrder(phoneNumber, customerAddress) {
+export async function sendOrder(phoneNumber, customerAddress) {
     console.log(`Order sent to ${phoneNumber} at ${customerAddress}`);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    throw new Error('failed to send order');
 }
--- sendOrder.ts
+++ sendOrder.ts
@@ -1,3 +1,5 @@
-export function sendOrder(phoneNumber: string, customerAddress: string) {
+export async function sendOrder(phoneNumber: string, customerAddress: string) {
     console.log(`Order sent to ${phoneNumber} at ${customerAddress}`);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    throw new Error('failed to send order');
 }
